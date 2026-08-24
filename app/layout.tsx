f79f4dea248b0343808831de0a362ef1ba34a960
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-08-24 15:26:29 +0530

    attached code for the homeopathy lp

--- app/layout.tsx
+++ app/layout.tsx
@@ -157,6 +157,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             })(window, document, "clarity", "script", "y6budyet2r");
           `}
         </Script>
+
+        {/* Facebook Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1734726727678449');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Meta Pixel Noscript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1734726727678449&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
