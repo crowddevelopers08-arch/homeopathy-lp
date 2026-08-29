689155073affeaadc000777bf94411ace38c0a64
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-08-29 17:14:15 +0530

    homeo both lps changes the gtagssekuys

--- app/layout.tsx
+++ app/layout.tsx
@@ -177,14 +177,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
 
-        {/* Microsoft Clarity */}
+        {/* Microsoft Clarity - Updated ID */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "y6budyet2r");
+            })(window, document, "clarity", "script", "y9wvumzsim");
           `}
         </Script>
 
